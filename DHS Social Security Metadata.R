ad608d71164f16914d689f92f2565b71a25b337d
BenCole6 <bencole@y7mail.com>
2018-10-03 15:14:56 +1000

    Update
    First real push

---
+++ DHS Social Security Metadata.R
@@ -0,0 +1,92 @@
+Metadata and caveats for all payments
+
+
+Confidentiality Provision
+
+All cells that have a value of less than 20, including zero, have been changed to display "<20". Not Available (N/A) will be inserted in other cells to avoid identifying customer numbers in "<20" cells. These rules have been employed for privacy reasons. This will prevent information from being broken down or manipulated to the degree that individuals may be identified, and to cover the legal requirement to protect an individual's privacy.
+
+Source: Unless otherwise noted the data is sourced from DSS Basic Data Set 
+
+Boundaries info 
+Customers who live overseas, those without a valid home address or who only have a postal address cannot be assigned to a boundary (LGA, CEB, Remote area etc.). These numbers may be represented as 'Unknown'. For this reason totals for payment recipients by boundaries will not match the State or National totals noting a person can be on more than one payment so totalling all the payment totals will not give you the total numbers of people of income support payments.
+
+Data Descriptions for the payments
+ABSTUDY (non living allowance), ABSTUDY (living allowance), Austudy, Newstart Allowance, Parenting Payment Partnered, Parenting Payment Single, Partner Allowance, Sickness Allowance, Widow Allowance, Youth Allowance (other), Youth Allowance (student), Youth Allowance (apprentice)
+Source: Department of Human Services administrative data (Blue Book dataset) Recipients of Parenting Payment Partnered/Parenting Payment Single/Youth Allowance/ABSTUDY/Austudy who are determined to be current (i.e. entitled to be paid) on the Centrelink payment system. 
+Recipients of Newstart Allowance who are determined to be current (i.e. entitled to be paid) on the Centrelink payment system and are not in receipt of CDEP Participation Supplement or a zero rate of payment. 
+Recipients of Sickness Allowance who are determined to be current (i.e. entitled to be paid) on the Centrelink payment system and are not in receipt of a zero rate of payment. 
+Recipients of Partner Allowance / Widow Allowance who are determined to be current (i.e. entitled to be paid) on the Centrelink payment system and are not in receipt of CDEP Participation Supplement or a zero rate of payment. 
+Commonwealth Electoral Divisions are based on the recipient's geo-coded address against the Australian Statistical Geography Standard (ASGS) 2013 
+
+Family Tax Benefits Part A and B (FTBA, FTBB) 
+Family Tax Benefit: 71% of all families with at least one dependent child aged <16 are eligible to receive FTB. Therefore, it should be viewed differently to other income support payments which by their targeted nature can help to identify segments of the population considered to be disadvantaged. FTB is too broadly targeted to serve that purpose.
+FTB Part A is received by approximately 67% of families with dependent children aged <16 who meet the basic conditions to receive the payment. Therefore, it should be viewed differently to other income support payments which by their targeted nature can help to identify segments of the population considered to be disadvantaged. FTB is too broadly targeted to serve that purpose.
+FTB Part B is received by approximately 59% of single parents and two parent families where one parent is on low income or not in paid employment with a dependent full time student up to the age of 18 years.
+FTB Maximum Rate: Where a tabulation shows FTB customers on the maximum rate, these generally have family incomes of less than $48,837 (for 2103/14). However, there is a large group of customers whose family incomes are less than $48,837 (for 2103/14) but who do not receive the maximum rate of FTB due to their receipt of child support income and the operation of the maintenance income test.
+FTB and Income Support Payments: Approximately one-third of FTB customers also receive an Income Support Payment and may be shown in both categories.
+Payment by Instalment versus Lump Sum: Information shown here in regard to FTB Part A & Part B refers only to fortnightly instalment customers. It excludes an additional 8% (approximately) who are paid through a lump sum which is claimable at the end of a financial year.
+Point-in-time data: This information represents the current population at a point in time and does not represent the total eligible population for the financial year.
+FTB Part A and Part B: Where the tabulation shows FTB Part A and Part B separately, approximately three-quarters of customers will appear in both categories as they are in receipt of both payments.
+Basic FTB Part A: Basic FTB Part A does not include Large Family Supplement, Multiple Birth Allowance or Rent Assistance.
+Family Tax Benefit is paid to the nominated partner in couple relationships. This means that reporting by gender is not like income support payments as the payment is made to the family. Where possible either relationship status will be reported instead, or simply no gender breakdown will be supplied.
+
+Age Pension 
+Income support payment for customers that have reached Age Pension age. The customer's Age Pension age depends on their date of birth: 
+If born before 1/7/52, Age Pension age is 65 
+If born between 1/7/52 and 31/12/53, Age Pension age is 65.5 
+If born between 1/1/54 and 30/06/55, Age Pension age is 66 
+If born between 1/7/55 and 31/12/56, Age Pension age is 66.5 
+If born from 1/1/57 or later, Age Pension age is 67. 
+Centrelink pays the vast majority of Age Pensions. Age pensioners who also receive a Disability Pension from the Department of Veterans' Affairs (DVA) have the choice of having their Age Pension paid by either DVA or Centrelink. Age Pension does not include recipients paid by the Department of Veterans' Affairs
+
+Disability Support Pension 
+DSP is an income support payment for people who are unable to work for 15 hours or more per week at or above the relevant minimum wage, independent of a Program of Support due to permanent physical, intellectual or psychiatric impairment. A DSP claimant must be aged 16 years or over and under Age Pension age at date of claim, however once in receipt of DSP, a person can continue to receive DSP beyond Age Pension age. Details on eligibility criteria are on the DHS website.
+State totals may not match other published data, due to differences in source data used for ad-hoc requests. 
+
+Carer Allowance 
+A supplementary payment for carers who provide daily care and attention at home for a person with a disability, severe medical condition or who is frail and aged. Carer Allowance may be paid in addition to income support payments. If a customer does not qualify for Carer Allowance (Child) based on the level of care required, the child they are caring for may still qualify for a Health Care Card if at least 14 hours a week of additional care and attention is provided. 
+Notes:
+Unless otherwise specified CA excludes HCC Only recipients and/or child care receivers.
+Due to a change in data source a break-in-series exists for Carer Allowance data from 1 July 2013. Care should be taken when comparing pre- and post- 1 July 2013 data.
+Carer Allowance data utilises a customers' home address as their primary address type to determine locational boundaries (e.g. State, SA, LGA, etc.), this may differ from other reported payments.
+State totals may not match other published data, due to differences in source data used for ad-hoc requests. 
+
+Carer Payment
+Carer Payment provides income support for carers who, because of the demands of their caring role, are unable to support themselves through substantial paid employment. 
+Notes:
+  Carer Payment totals include overseas customers.
+Due to a change in data source a break-in-series exists for Carer Payment data from 1 July 2013. Care should be taken when comparing pre- and post- 1 July 2013 data.
+Carer Payment data utilises a customers' home address as their primary address type to determine locational boundaries (e.g. State, SA, LGA, etc.), this may differ from other reported payments.
+State totals may not match other published data, due to differences in source data used for ad-hoc requests. 
+
+Special Benefit (SpB)
+An income support payment for customers who are in severe financial hardship due to circumstances beyond their control and who are ineligible for any other income support payment. Includes only those customers whose benefit status is 'Current'.
+State totals may not match other published data, due to differences in source data used for ad-hoc requests. 
+
+Wife Pension - Age and Disability (WFA, WFD) 
+An income support payment for the female partner of an age Age or Disability Support pensioner. There have been no new grants of Wife Pension since 1 July 1995. 
+Current recipients remain eligible to receive this payment until otherwise disqualified from receiving it, or until they reach age pension age 
+
+Double Orphan Pension
+The objective of Double Orphan Pension is to make non-means-tested payments to guardians or approved care organisations to assist in meeting the costs of dependent children, in cases where both parents have died or one parent has died and the other parent is unable or unavailable to care for the child.
+Widow B pension (WID B) 
+An income support payment to ensure customers have an adequate level of income if they are an older widow who does not qualify for parenting payment, have limited needs, and have lost the financial support of their partner. There have been no new grants of Widow B Pension since 20 March 1997. 
+Current Wid B recipients remain eligible to receive this payment until otherwise disqualified from receiving it, or until they reach age pension age. The majority of Widow B Pension recipients (around 95 per cent) live overseas and could not be assigned to an electorate.
+
+Pension Concession Card (PCC)
+The PCC is automatically issued to pensioners and eligible allowance recipients to provide access to health concessions, including cheaper pharmaceutical medicines.
+
+Commonwealth Seniors Health Card (CSHC)
+The CSHC is claimed by people of Age Pension age who do not receive a pension, but meet an adjusted taxable income test. It provides access to health concessions, including cheaper pharmaceutical medicines as well as an entitlement to the Seniors Supplement.
+
+Low Income Health Care Card (LIC)
+A card which can be claimed by low income earners, providing them access to health concessions, including cheaper pharmaceutical medicines. 
+
+General (HCC + LIC)
+HCC numbers include those with a low income card (LIC). Do not add LIC to HCC.
+
+Health Care Card (HCC)
+The HCC is issued to recipients of certain social security benefit or allowance payments to provide access to health concessions, including cheaper pharmaceutical medicines. Low-income earners not receiving benefits or allowances may claim the HCC 
+
+Concession Cards by Payment type 
+Certain payments automatically attract more than one concession card entitlement in particular circumstances (e.g. an initial HCC, then a PCC at a later time). As a result, double counting may occur.
